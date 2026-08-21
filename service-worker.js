1ef7ccc52c6dd565de1060803e2bb11143fa047b
Hakuryu0128 <arita-rs@c.bears.ed.jp>
2026-08-21 10:24:39 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'weeky-H19';
-const CORE = ['./', './index.html', './styles.css?v=H19', './app.js?v=H19', './manifest.webmanifest'];
+const CACHE_NAME = 'weeky-H20';
+const CORE = ['./', './index.html', './styles.css?v=H20', './app.js?v=H20', './manifest.webmanifest'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).catch(() => null)); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => {
